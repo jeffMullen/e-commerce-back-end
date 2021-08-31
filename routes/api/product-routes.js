e4795e8ec5f3315682afbf88c9482057ab1aa9c8
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-08-30 20:16:52 -0400

    tag routes complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,8 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll({
+      include: [Category, Tag]
+    })
+    res.status(200).json(productData);
+
+  } catch {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
@@ -89,8 +98,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(productData);
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,78 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
+  try {
+    const tagData = await Tag.findAll({
+      include: Product
+    })
+    res.status(200).json(tagData);
+  } catch {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: Product
+    })
+    if (!tagData) {
+      res.status(404).json("ID doesn't exist!");
+    }
+    res.status(200).json(tagData);
+  } catch {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch {
+    res.status(500).json(tagData);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+    res.status(200).json('Tag Updated!');
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!tagData) {
+      res.status(404).json('Tag not found')
+    }
+    res.status(200).json('Tag Deleted!');
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
